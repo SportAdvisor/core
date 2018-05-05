3002dffa80ef66aac7c1f0e776d07d4d4112fbf5
Vladimir Alekseev <som3t1me5@gmail.com>
2018-05-05 21:36:48 +0300

    issue #15 circle config (use embedded pg)

--- src/test/scala/io/sportadvisor/InMemoryPostgresStorage.scala
+++ src/test/scala/io/sportadvisor/InMemoryPostgresStorage.scala
@@ -1,12 +1,8 @@
 package io.sportadvisor
 
-import java.nio.file.Paths
-
 import de.flapdoodle.embed.process.runtime.Network._
 import io.sportadvisor.util.db.{DatabaseConnector, DatabaseMigration}
-import ru.yandex.qatools.embed.postgresql.{EmbeddedPostgres, PostgresStarter}
-import ru.yandex.qatools.embed.postgresql.config.AbstractPostgresConfig.{Credentials, Net, Storage, Timeout}
-import ru.yandex.qatools.embed.postgresql.config.PostgresConfig
+import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres
 import ru.yandex.qatools.embed.postgresql.distribution.Version
 
 /**
@@ -20,15 +16,7 @@ object InMemoryPostgresStorage {
   val dbPassword = "password"
   val jdbcUrl = s"jdbc:postgresql://$dbHost:$dbPort/$dbName"
 
-  val psqlConfig = new PostgresConfig(
-    Version.V9_6_8, new Net(dbHost, dbPort),
-    new Storage(dbName, Paths.get(System.getProperty("java.io.tmpdir"), "pgembed").toAbsolutePath.toString),
-    new Timeout(30000),
-    new Credentials(dbUser, dbPassword)
-  )
-//  val psqlInstance = PostgresStarter.getDefaultInstance
   val flywayService = new DatabaseMigration(jdbcUrl, dbUser, dbPassword)
-//  val process = psqlInstance.prepare(psqlConfig).start()
   val process: String = new EmbeddedPostgres(Version.V9_6_8).start(dbHost, dbPort, dbName, dbUser, dbPassword)
   println(process)
   flywayService.dropDatabase()

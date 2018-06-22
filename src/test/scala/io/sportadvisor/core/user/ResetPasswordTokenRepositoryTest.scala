af623edd5e8f8ffc48b2ec72d203be30210bcb1a
szinner <salavat.zinn@gmail.com>
2018-06-22 16:26:35 +0300

    core-6-reset-pwd reset token test

---
+++ src/test/scala/io/sportadvisor/core/user/ResetPasswordTokenRepositoryTest.scala
@@ -0,0 +1,33 @@
+package io.sportadvisor.core.user
+
+import java.time.LocalDateTime
+
+import io.sportadvisor.{BaseTest, InMemoryPostgresStorage}
+
+import scala.concurrent.Future
+
+class ResetPasswordTokenRepositoryTest extends BaseTest {
+
+  "ResetPasswordTokenRepositorySQL" when {
+
+    "save and get" should {
+      "successful save" in new Context {
+        val f: Future[ResetPasswordToken] =
+          resetTokenRepository.save(ResetPasswordToken(testToken, LocalDateTime.now().plusDays(1)))
+        awaitForResult(f)
+        val token: Option[ResetPasswordToken] = awaitForResult(resetTokenRepository.get(testToken))
+        token.isDefined shouldBe true
+      }
+    }
+
+  }
+
+
+  trait Context {
+    val testToken = "token"
+
+    val resetTokenRepository: ResetPasswordTokenRepository = new ResetPasswordTokenRepositorySQL(
+      InMemoryPostgresStorage.databaseConnector)
+  }
+
+}

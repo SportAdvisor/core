46df81aee5b16d051cca2bcb36356f621459341c
Alekseev Vladimir <som3t1me5@gmail.com>
2018-06-23 08:45:21 +0300

    issue #7 configure MailContainer for e2e tests & ChangeMail test (#48)

--- src/e2e/scala/io/sportadvisor/MailContainer.scala
+++ src/e2e/scala/io/sportadvisor/MailContainer.scala
@@ -1,21 +1,78 @@
 package io.sportadvisor
+
 import com.dimafeng.testcontainers._
+import io.circe.generic.semiauto._
+import io.circe.Decoder
+import io.circe.parser._
+import scalaj.http.Http
 
 /**
   * @author sss3 (Vladimir Alekseev)
   */
 trait MailContainer extends BaseE2ETest {
 
-  lazy val mailContainer: Container =
-    GenericContainer("mailhog/mailhog:latest", exposedPorts = Seq(smtpPort), env = smtpEnv())
+  private val smptPort = 1025
+
+  lazy val mailContainer: GenericContainer =
+    GenericContainer("mailhog/mailhog:latest", env = smtpEnv())
+  lazy val env: Map[String, String] = Map(
+    SMTP_IP -> getIp(mailContainer.container.getContainerInfo),
+    SMTP_PORT -> smptPort.toString,
+    MAIL_HOG_API_PORT -> 8025.toString
+  )
 
   override def additionalContainer(
       initial: Seq[LazyContainer[Container]]): Seq[LazyContainer[Container]] = {
-    val seq = Seq(LazyContainer(mailContainer)) ++ initial
+    val seq = Seq(LazyContainer(mailContainer: Container)) ++ initial
     super.additionalContainer(seq)
   }
 
   private def smtpEnv(): Map[String, String] = Map(
     "MH_HOSTNAME" -> "sportadvisor.io"
   )
+
+  override protected def mailEnv: Map[String, String] = env
+
+  def messages(): Seq[Message] = {
+    mhMessages map (Message(_))
+  }
+
+  final case class Message(from: String, to: Seq[String], body: String, subject: String)
+
+  private object Message {
+    def apply(mh: MailhogMessage): Message = {
+      val from = mapPath(mh.From)
+      val to = mh.To.map(mapPath)
+      val body = mh.Content.Body
+      new Message(from, to, body, mh.Content.Headers.Subject.head)
+    }
+
+    def mapPath: Path => String = p => s"${p.Mailbox}@${p.Domain}"
+  }
+
+  private def mhMessages(): Seq[MailhogMessage] = {
+    val resp = Http(s"http://${mailEnv(SMTP_IP)}:${mailEnv(MAIL_HOG_API_PORT)}/api/v2/messages").asString
+    parse(resp.body) match {
+      case Left(e) => throw new IllegalStateException(e)
+      case Right(root) =>
+        respDecoder.decodeJson(root) match {
+          case Left(e)  => throw new IllegalStateException(e)
+          case Right(r) => r.items
+        }
+    }
+  }
+
+  private final case class ApiResponse(items: Seq[MailhogMessage])
+  private final case class MailhogMessage(From: Path, To: Seq[Path], Content: ContentValue)
+
+  private final case class Path(Mailbox: String, Domain: String)
+  private final case class ContentValue(Body: String, Headers: HeaderValue)
+  private final case class HeaderValue(Subject: Seq[String])
+
+  private implicit val pathDecoder: Decoder[Path] = deriveDecoder
+  private implicit val contentDecoder: Decoder[ContentValue] = deriveDecoder
+  private implicit val headerDecoder: Decoder[HeaderValue] = deriveDecoder
+  private implicit val messageDecoder: Decoder[MailhogMessage] = deriveDecoder
+  private implicit val respDecoder: Decoder[ApiResponse] = deriveDecoder
+
 }
---
+++ src/e2e/scala/io/sportadvisor/http/user/DefaultUsersData.scala
@@ -0,0 +1,39 @@
+package io.sportadvisor.http.user
+
+import java.sql.DriverManager
+
+import com.roundeights.hasher.Implicits._
+import io.sportadvisor.core.user.UserData
+import io.sportadvisor.{BaseE2ETest, Preconditions}
+
+/**
+  * @author sss3 (Vladimir Alekseev)
+  */
+trait DefaultUsersData extends Preconditions { self: BaseE2ETest =>
+
+  protected var user: UserData = UserData(1L, "", "", "", Some(""))
+
+  override def setup(): Unit = {
+    Class.forName(pgContainer.driverClassName)
+    val password = "str0nGpass"
+    val email = "vladimir@sportadvisor.io"
+    val name = "Vladimir"
+    val lang = "en"
+    val connection =
+      DriverManager.getConnection(pgContainer.jdbcUrl, pgContainer.username, pgContainer.password)
+    val insertSql =
+      s"""insert into public."ACCOUNTS" (name, email, password, language) VALUES('$name', '$email', '${password.sha256.hex}'
+         |, '$lang') """.stripMargin
+    connection.prepareStatement(insertSql).execute()
+    val rs = connection
+      .prepareStatement(s"""select id from public."ACCOUNTS" where email = '$email'""")
+      .executeQuery()
+    rs.next()
+    val id = rs.getLong(1)
+
+    connection.close()
+    user = UserData(id, email, password, name, Some(lang))
+    super.setup()
+  }
+
+}

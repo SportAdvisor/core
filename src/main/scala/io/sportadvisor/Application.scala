04b85de47bb42bbb7d3181cef022cdcbbc4a0ae7
Vladimir Alekseev <som3t1me5@gmail.com>
2018-06-24 23:56:46 +0300

    fix several errors

--- src/main/scala/io/sportadvisor/Application.scala
+++ src/main/scala/io/sportadvisor/Application.scala
@@ -1,6 +1,6 @@
 package io.sportadvisor
 
-import akka.actor.{ActorSystem, Cancellable}
+import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.directives.DebuggingDirectives
@@ -17,9 +17,10 @@ import scala.concurrent.duration._
 /**
   * @author sss3 (Vladimir Alekseev)
   */
-object Application extends App {
+object Application {
 
-  def startApplication(): Unit = {
+  @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+  def main(args: Array[String]): Unit = {
     implicit val actorSystem: ActorSystem = ActorSystem()
     implicit val executor: ExecutionContext = actorSystem.dispatcher
     implicit val materializer: ActorMaterializer = ActorMaterializer()
@@ -45,15 +46,11 @@ object Application extends App {
     val httpRoute = new HttpRoute(usersService)
 
     val tokenCleaner = new TokenCleaner(tokenRepository)
-    val cancellable: Cancellable =
-      actorSystem.scheduler.schedule(12.hour, 3.hour)(tokenCleaner.clean())
+    actorSystem.scheduler.schedule(12.hour, 3.hour)(tokenCleaner.clean())
 
     val clientRouteLogged =
       DebuggingDirectives.logRequestResult("request tracer" -> Logging.InfoLevel)(httpRoute.route)
     val _: Future[_] = Http().bindAndHandle(clientRouteLogged, config.http.host, config.http.port)
-    ()
   }
-
-  startApplication()
 
 }
--- src/main/scala/io/sportadvisor/util/db/DatabaseMigration.scala
+++ src/main/scala/io/sportadvisor/util/db/DatabaseMigration.scala
@@ -11,7 +11,7 @@ class DatabaseMigration(jdbcUrl: String, dbUser: String, dbPassword: String) {
   private val flyway = new Flyway()
   flyway.setDataSource(jdbcUrl, dbUser, dbPassword)
 
-  def migrateDatabaseSchema(): Unit = flyway.migrate()
+  def migrateDatabaseSchema(): Int = flyway.migrate()
 
   def dropDatabase(): Unit = flyway.clean()
 

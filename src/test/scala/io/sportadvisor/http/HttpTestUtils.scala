699cd1bedf50a7553e6b744cd7769536fed92426
Vladimir Alekseev <som3t1me5@gmail.com>
2018-07-11 05:54:24 +0300

    fix test

--- src/test/scala/io/sportadvisor/http/HttpTestUtils.scala
+++ src/test/scala/io/sportadvisor/http/HttpTestUtils.scala
@@ -4,21 +4,17 @@ import akka.http.scaladsl.model.HttpHeader
 import akka.http.scaladsl.model.HttpHeader.ParsingResult
 import io.sportadvisor.core.auth.AuthService
 import io.sportadvisor.core.user.UserModels.UserID
-import org.mockito.Matchers.anyString
 import org.mockito.Mockito.when
-
-import scala.util.Random
 
 /**
   * @author sss3 (Vladimir Alekseev)
   */
 object HttpTestUtils {
 
-  private val headerLen = 10
-
   def authHeader(userID: UserID)(implicit authService: AuthService): HttpHeader = {
-    when(authService.userId(anyString)).thenReturn(Option(userID))
-    HttpHeader.parse(authorizationHeader, Random.nextString(headerLen)) match {
+    val token = "asd.asd.asd"
+    when(authService.userId(token)).thenReturn(Option(userID))
+    HttpHeader.parse(authorizationHeader, token) match {
       case ParsingResult.Ok(h, _) => h
       case ParsingResult.Error(e) => throw new IllegalArgumentException(e.formatPretty)
     }

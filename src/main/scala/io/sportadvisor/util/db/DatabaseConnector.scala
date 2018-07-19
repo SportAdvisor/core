93e220453654471188ca5c8185aa4e97b9ead4a5
Alekseev Vladimir <som3t1me5@gmail.com>
2018-07-19 14:28:26 +0300

    fix potential deadlock (#66)
    https://app.asana.com/0/687137397635706/748807124957853

--- src/main/scala/io/sportadvisor/util/db/DatabaseConnector.scala
+++ src/main/scala/io/sportadvisor/util/db/DatabaseConnector.scala
@@ -7,18 +7,22 @@ import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
   */
 class DatabaseConnector(jdbcUrl: String, dbUser: String, dbPassword: String) {
 
+  private val maxConnections = 20
   private val hikariDataSource = {
     val hikariConfig = new HikariConfig()
     hikariConfig.setJdbcUrl(jdbcUrl)
     hikariConfig.setUsername(dbUser)
     hikariConfig.setPassword(dbPassword)
-
+    hikariConfig.setMaximumPoolSize(maxConnections)
     new HikariDataSource(hikariConfig)
   }
 
   val profile: SAPostgresProfile = SAPostgresProfile
   import profile.api._
 
-  val db: profile.backend.DatabaseDef = Database.forDataSource(hikariDataSource, None)
+  val db: profile.backend.DatabaseDef = Database.forDataSource(
+    hikariDataSource,
+    Option(maxConnections),
+    AsyncExecutor.default("SlickExecutor.SA", maxConnections))
   val session: profile.backend.SessionDef = db.createSession()
 }

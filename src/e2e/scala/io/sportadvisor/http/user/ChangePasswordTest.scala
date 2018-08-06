ec6dccfd676b744caac2d14e13c28d3472a17115
szinner <salavat.zinn@gmail.com>
2018-08-06 11:57:33 +0300

    fixes

--- src/e2e/scala/io/sportadvisor/http/user/ChangePasswordTest.scala
+++ src/e2e/scala/io/sportadvisor/http/user/ChangePasswordTest.scala
@@ -44,12 +44,12 @@ class ChangePasswordTest extends BaseE2ETest with UserMappings with DefaultUsers
       req("sign-in"),
       s"""{"email": "${user.email}", "password": "${user.password}", "remember":true}""").asString
     firsSignInResp.code shouldBe OK.intValue
-
     val token = r[DataResponse[AuthToken, ObjectData[AuthToken]]](firsSignInResp.body).data.data.token
 
     val newPassword = user.password + "1"
     val response = put(req(user.id + "/password"),
-                       s"""{"password": "${user.password}1", "newPassword": "$newPassword" }""", token).asString
+                       s"""{"password": "${user.password}1", "newPassword": "$newPassword" }""",
+                       token).asString
     response.code shouldBe BadRequest.intValue
 
     val body = r[ErrorResponse[FormError]](response.body)
@@ -62,17 +62,17 @@ class ChangePasswordTest extends BaseE2ETest with UserMappings with DefaultUsers
       req("sign-in"),
       s"""{"email": "${user.email}", "password": "${user.password}", "remember":true}""").asString
     firsSignInResp.code shouldBe OK.intValue
-
     val token = r[DataResponse[AuthToken, ObjectData[AuthToken]]](firsSignInResp.body).data.data.token
 
     val newPassword = "weak"
     val response = put(req(user.id + "/password"),
-                       s"""{"password": "${user.password}", "newPassword": "$newPassword" }""", token).asString
+                       s"""{"password": "${user.password}", "newPassword": "$newPassword" }""",
+                       token).asString
     response.code shouldBe BadRequest.intValue
 
     val body = r[ErrorResponse[FormError]](response.body)
     body.code shouldBe BadRequest.intValue
-    body.errors should (contain(FormError("password", UserRouteValidators.passwordIsWeak)) and have size 1)
+    body.errors should (contain(FormError("newPassword", UserRouteValidators.passwordIsWeak)) and have size 1)
   }
 
   "PUT /api/users/{id}/password to another user" should "return 403 code" in {
@@ -80,29 +80,28 @@ class ChangePasswordTest extends BaseE2ETest with UserMappings with DefaultUsers
       req("sign-in"),
       s"""{"email": "${user.email}", "password": "${user.password}", "remember":true}""").asString
     firsSignInResp.code shouldBe OK.intValue
-
     val token = r[DataResponse[AuthToken, ObjectData[AuthToken]]](firsSignInResp.body).data.data.token
 
     val newPassword = user.password + "1"
     val userId = user.id + 1
     val response = put(req(userId + "/password"),
-                       s"""{"password": "${user.password}", "newPassword": "$newPassword" }""", token).asString
+                       s"""{"password": "${user.password}", "newPassword": "$newPassword" }""",
+                       token).asString
     response.code shouldBe Forbidden.intValue
 
-    val body = r[ErrorResponse[FormError]](response.body)
+    val body = r[EmptyResponse](response.body)
     body.code shouldBe Forbidden.intValue
-    body.errors should (contain(FormError("password", UserRoute.authError)) and have size 1)
   }
 
   "PUT /api/users/{id}/password unauthorized request" should "return 401 code" in {
     val newPassword = user.password + "1"
-    val response = put(req(user.id + "/password"),
+    val response = put(
+      req(user.id + "/password"),
       s"""{"password": "${user.password}", "newPassword": "$newPassword" }""").timeout(10.seconds).asString
     response.code shouldBe Unauthorized.intValue
 
     val body = r[EmptyResponse](response.body)
     body.code shouldBe Unauthorized.intValue
-
   }
 
 }
